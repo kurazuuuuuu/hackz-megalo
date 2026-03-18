42caf75cbec471c26e76ccd5714a47cabf6fbaac
kurazuuuuuu <110393398+kurazuuuuuu@users.noreply.github.com>
2026-03-18 13:18:28 +0900

    change: シークレットの同期間隔を変更

--- terraform/gcp/deploy/secret_sync.tf
+++ terraform/gcp/deploy/secret_sync.tf
@@ -1,11 +1,11 @@
 resource "terraform_data" "enable_secret_sync" {
   triggers_replace = [
     google_container_cluster.deploy.id,
-    "300s"
+    "60s"
   ]
 
   provisioner "local-exec" {
-    command = "gcloud beta container clusters update ${var.gke_cluster_name} --project=${var.project_id} --location=${var.region} --enable-secret-sync --enable-secret-sync-rotation --secret-sync-rotation-interval=300s"
+    command = "gcloud beta container clusters update ${var.gke_cluster_name} --project=${var.project_id} --location=${var.region} --enable-secret-sync --enable-secret-sync-rotation --secret-sync-rotation-interval=60s"
   }
 
   depends_on = [
